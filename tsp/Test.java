ff83d0e7cf6fca8d6b865e7e2e30038df9297241
busterBCB <38755865+busterBCB@users.noreply.github.com>
2018-05-19 17:18:02 +0200

    Update Test.java

--- tsp/Test.java
+++ tsp/Test.java
@@ -1,58 +1,11 @@
-
 package tsp;
-
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
-
 
 public class Test {
 
-   
     public static void main(String[] args) {
-       /*
-        Product kaas = new Product(1 , 5);
-        Product melk = new Product(3 , 4);
-        Product boter = new Product(2 , 4);
-        Product ei = new Product(1 , 4);
-        Product spek = new Product(5 , 3);
-        Product brood = new Product(4 , 2);
-        Product mayo = new Product(5 , 1);
-        Product kaas = new Product(3 , 5);
-        Product melk = new Product(3 , 2);
-        Product boter = new Product(1 , 4);
-        Product ei = new Product(1 , 2);
-        Product spek = new Product(5 , 3);
-        
-        
-        ProductList PList = new ProductList();
-        PList.AddProduct(kaas);
-        PList.AddProduct(melk);
-        PList.AddProduct(boter);
-        PList.AddProduct(ei);
-        PList.AddProduct(spek);
-        PList.AddProduct(brood);
-        PList.AddProduct(mayo);
-        //System.out.println(PList);
-        
-        Greedy A1 = new Greedy(PList.getList());
-        Result R1 = A1.BerekenRoute();
-        System.out.println(R1);
-        
-        LimitedBruteForce Brute = new LimitedBruteForce(PList.getList());
-        Result R2 = Brute.BerekenRoute();
-        System.out.println(R2);
 
-        II_opt opt = new II_opt(PList.getList());
-        Result R3 = opt.BerekenRoute();
-        System.out.println(R3);*/
-        
         Hoofdscherm scherm = new Hoofdscherm();
         scherm.setVisible(true);
-      
-        
-        }
-    }
-    
 
+    }
+}
