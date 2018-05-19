09e5ed0bc0e618ad40e54d3d1c55373d1b8e7c25
busterBCB <38755865+busterBCB@users.noreply.github.com>
2018-05-19 17:11:11 +0200

    Update Greedy.java

--- tsp/Greedy.java
+++ tsp/Greedy.java
@@ -17,34 +17,34 @@ public class Greedy extends Algorithm {
     public Result BerekenRoute(ArrayList<Product> P) {
         Products = (ArrayList<Product>)P.clone();
         Route.clear();
+        afstand = 0;
         long startTime = System.nanoTime();
         int x;
         int y;
         double klein = 20;
-        double lengte = 0;
         for (Product temp : Products) {
             x = temp.getX();
             y = temp.getY();
             double lijn = Math.sqrt((x * x) + (y * y));
+            //kortste onthouden
             if (lijn < klein) {
                 klein = lijn;
                 if (Route.size() == 0) {
                     Route.add(temp);
                     laatste = temp;
-                    lengte = lijn;
                 } else {
                     Route.set(0, temp);
                     laatste = temp;
-                    lengte = lijn;
                 }
             }
         }
-        afstand = afstand + lengte;
+        afstand = afstand + klein;
         Products.remove(laatste);
         klein = 20;
         double lijn;
         int aantalRoute = 1;
         int aantal = 0;
+        // tot alle producten verwijderd zijn
         for (int i = 0; i < Products.size();) {
             for (Product temp : Products) {                    
                     x = temp.getX();
@@ -61,35 +61,24 @@ public class Greedy extends Algorithm {
                     if (lijn < klein) {
                         klein = lijn;
                         if (Route.size() == aantalRoute) {
-                            lengte = lijn;
                             Route.add(temp);
                         } else {
-                            lengte = lijn;
                             Route.set(aantalRoute, temp);
                         }
                     }
                 
             }
-            afstand = afstand + lengte;
+            afstand = afstand + klein;
             klein = 20;
             Products.remove(Route.get(aantalRoute));
             aantalRoute++;
             aantal++;
         }
+        // laatste product naar de start
         x = Route.get(Route.size()-1).getX();
         y = Route.get(Route.size()-1).getY();
         afstand = afstand + Math.sqrt((x * x) + (y * y));
         Rekentijd = System.nanoTime() - startTime;
-        return new Result(Route, afstand, Rekentijd);
+        return new Result((ArrayList<Product>)Route.clone(), afstand, Rekentijd, "Greedy");
     }
-
-    /*@Override
-    public String toString() {
-        return "Greedy{" + "Route=" + Route + ", afstand=" + afstand + ", Rekentijd=" + Rekentijd + '}';
-    }*/
-
-    
-
-    
-
 }
